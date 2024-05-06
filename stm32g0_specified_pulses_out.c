7b8378063d205cc45f88c2fbc747318c2f046e00
asad12asad <56396632+asad12asad@users.noreply.github.com>
2024-05-06 23:39:44 +0500

    Create stm32g0_specified_pulses_out.c
    /*
    Description:
    This C file contains functions to generate a specific number of pulses using Timer 14 (TIM14) and Timer 3 (TIM3) peripherals on an STM32G030 microcontroller. The pulses are generated at a 1Hz frequency with a duty cycle of 50%.
    
    1. Variables:
       - prescaler: Prescaler value for Timer 14 to generate a 1kHz clock.
       - arr_value: Auto-reload value for Timer 14 to achieve a 1Hz frequency (1 pulse per second).
       - duty_cycle: Duty cycle percentage for the PWM signal generated by Timer 14.
    
    2. tim3_pulse_counter_init():
       - Initializes Timer 3 to count pulses.
       - Enables Timer 3 clock.
       - Configures Timer 3 in slave mode with ITR3 (internal trigger input 3) as the trigger input.
       - Sets Timer 3 to count up on each trigger.
       - Sets the auto-reload value to the desired pulse count.
       - Enables update interrupt for Timer 3.
       - Configures NVIC for Timer 3 interrupt.
       - Starts Timer 14.
    
    3. tim14_init():
       - Initializes Timer 14 for PWM generation.
       - Enables GPIOA clock.
       - Configures PA7 as an alternate function mode to output PWM signal.
       - Sets PA7 to AF4 (TIM14_CH1) to use Timer 14 as a PWM source.
       - Enables Timer 14 clock.
       - Configures Timer 14 with the specified prescaler, auto-reload value, and PWM mode.
       - Sets the compare value for duty cycle.
       - Enables capture/compare output for Timer 14.
       - Generates an update event to apply settings.
       - Enables Timer 14 counter.
    
    4. TIM3_IRQHandler():
       - Interrupt service routine for Timer 3 update event.
       - Toggles GPIOA output pin to indicate pulse count reached.
       - Clears the update interrupt flag of Timer 3..
    */

---
+++ stm32g0_specified_pulses_out.c
@@ -0,0 +1,88 @@
+/**
+  ******************************************************************************
+  * @file    specified_pulses_out.c
+  * @author  asadshafi
+  * @brief   Description of the file.
+  *
+  * @details Detailed description of the file, including purpose and usage.
+  * This C file contains functions to generate a specific number of pulses using 
+  * Timer 14 (TIM14) and Timer 3 (TIM3) peripherals on an STM32G030
+  * microcontroller.
+  * TIM3_IRQHandler():
+  * - Interrupt service routine for Timer 3 update event.
+  * Perform relevant logic to stop pulses 
+  * @note    Additional notes or considerations.
+  ******************************************************************************
+  */
+
+
+#include "stm32g030xx.h"
+#include "stm32g0xx.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+uint16_t prescaler = 700 - 1; // Set prescaler for 1kHz clock
+uint16_t arr_value = 100 - 1; // Set auto-reload value for 1Hz frequency (1 pulse per second)
+uint16_t duty_cycle = 50;     // Set duty cycle to 50% (0-1000 represents 0-100%)
+/**
+  * @brief  Initializes Timer 3 to count pulses.
+  * @param  pulse_count: Desired number of pulses to count.
+  * @retval None
+  */
+void tim3_pulse_counter_init(uint16_t pulse_count)
+{
+    // 1. Enable TIM3 and TIM14 clocks
+    RCC->APBENR1 |= RCC_APBENR1_TIM3EN;
+    // RCC->APBENR1 |= RCC_APBENR1_TIM14EN;
+
+    // 2. Configure TIM3 in slave mode with ITR3 as trigger input
+    TIM3->SMCR |= TIM_SMCR_SMS_2 | TIM_SMCR_SMS_1 | TIM_SMCR_SMS_0 | TIM_SMCR_TS_0 | TIM_SMCR_TS_1; // Trigger mode, ITR2 selected
+
+    // 3. Configure TIM3 to count upon each trigger
+    TIM3->CR1 |= TIM_CR1_CEN; // Enable TIM3 counter
+
+    // 4. Set the auto-reload value to the desired pulse count
+    TIM3->ARR = pulse_count - 1;
+
+    // 5. Enable update interrupt for TIM3
+    TIM3->DIER |= TIM_DIER_UIE;
+
+    // 6. Configure NVIC for TIM3 interrupt
+    NVIC_EnableIRQ(TIM3_IRQn);
+
+    // 7. Start TIM14
+    TIM14->CR1 |= TIM_CR1_CEN;
+}
+/**
+  * @brief  Initializes Timer 14 for PWM generation.
+  * @retval None
+  */
+void tim14_init()
+{
+
+    // Enable TIM14 clock
+    RCC->APBENR2 |= RCC_APBENR2_TIM14EN;
+
+    // Configure TIM14
+    TIM14->PSC = prescaler;
+    TIM14->ARR = arr_value;
+    TIM14->CCMR1 &= ~TIM_CCMR1_OC1M;                     // Clear output compare mode
+    TIM14->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2; // Set to PWM mode 1
+    TIM14->CCR1 = duty_cycle;                            // Set compare value for duty cycle
+    TIM14->CCER |= TIM_CCER_CC1E;                        // Enable capture/compare output
+    TIM14->EGR |= TIM_EGR_UG;                            // Generate update event to apply settings
+
+    // Enable TIM14 counter
+    TIM14->CR1 |= TIM_CR1_CEN;
+}
+void TIM3_IRQHandler(void)
+{
+    if (TIM3->SR & TIM_SR_UIF)
+    {
+        // Pulse count reached, perform actions here
+        // TIM14->CR1 &= ~TIM_CR1_CEN;
+
+        GPIOA->ODR ^= (0x7f & ~sevenSegment[2]);
+        TIM3->SR &= ~TIM_SR_UIF; // Clear update interrupt flag
+    }
+}
